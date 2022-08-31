c2118add86ac8ddbd0b741c5f71348fc03248ecc
sausukei <ikenosousuke@icloud.com>
2022-08-31 17:08:02 +0900

    Update nligraph.py

--- hokudai-intern2022/nligraph.py
+++ hokudai-intern2022/nligraph.py
@@ -1,10 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import glob
+import os 
+import re
 
-
-for filename in glob.glob('mainpeople_newanalyse/*.txt'):
-   print(filename.replace('mainpeople_newanalyse/',''))
+for filename in glob.glob('nli/nli*.txt'):
+   print(filename.replace('nli/nli',''))
    with open(os.path.join(os.getcwd(), filename), 'r') as f:
        
-       cut = str(filename.replace('mainpeople_newanalyse/',''))
+       cut = str(filename.replace('nli/nli',''))
+       print(cut)
